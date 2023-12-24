799d08a6d406eab429dd2a01c661018bb25cc37a
VIGNESHKUMAR RADHAKRISHNAN <vigneshkumarponnamaravathy@gmail.com>
2023-12-24 18:40:30 +0530

    Time: 15 ms (85.72%), Space: 42.1 MB (65.52%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -2,10 +2,8 @@ class Solution {
     public String longestPalindrome(String s) {
         int n = s.length();
         int start = 0;
-        int maxElement = 1; // because 1 palindrome and even/odd return sub string l to r
-        int count = 0;
+        int maxElement = 1;
         for(int i=1;i<n;i++){
-            //if even palindromic then current and previous
             int l = i-1;
             int r = i;
             while(l >= 0 && r<n && s.charAt(l) == s.charAt(r)){
@@ -16,7 +14,6 @@ class Solution {
                 l--;
                 r++;
             }
-            //if odd palindromic, then previous i-1, center i and next i+1
             l = i-1;
             r= i+1;
             while(l >=0 && r<n && s.charAt(l) == s.charAt(r)){

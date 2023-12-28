17e1d81d9ecbe1a400e382b565e6e24a13608672
VIGNESHKUMAR RADHAKRISHNAN <vigneshkumarponnamaravathy@gmail.com>
2023-12-28 22:57:42 +0530

    Time: 1 ms (46.80%), Space: 44.5 MB (18.63%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[][] floodFill(int[][] image, int sr, int sc, int targetColor) {
+        int currentColor = image[sr][sc];
+        if(currentColor == targetColor)
+            return image;
+        dfs(image, sr, sc,targetColor, currentColor);
+        return image;
+    }
+    private void dfs(int[][] image, int sr, int sc, int targetColor, int currentColorNeedsToPaint){
+        if(sr < 0|| sc<0|| sr >= image.length || sc >= image[0].length || targetColor == currentColorNeedsToPaint || image[sr][sc] != currentColorNeedsToPaint)
+            return;
+        image[sr][sc] = targetColor;
+        dfs(image, sr+1, sc, targetColor, currentColorNeedsToPaint);
+        dfs(image, sr-1, sc,targetColor, currentColorNeedsToPaint);
+        dfs(image, sr, sc+1,targetColor, currentColorNeedsToPaint);
+        dfs(image, sr, sc-1,targetColor, currentColorNeedsToPaint);
+    }
+}

ec8817486f3dfb569e301e7c23a0fc894adcc1a0
VIGNESHKUMAR RADHAKRISHNAN <vigneshkumarponnamaravathy@gmail.com>
2023-12-28 22:39:59 +0530

    Time: 1 ms (84.91%), Space: 44.4 MB (7.15%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.java
+++ 0841-keys-and-rooms/0841-keys-and-rooms.java
@@ -1,10 +1,9 @@
 class Solution {
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
-        int n = rooms.size();
-        boolean[] visited = new boolean[n];
+        boolean[] visited = new boolean[rooms.size()];
         dfs(rooms, visited, 0);
-        for(boolean visitedOrNot: visited){
-            if(!visitedOrNot)
+        for(boolean visit: visited){
+            if(!visit)
                 return false;
         }
         return true;

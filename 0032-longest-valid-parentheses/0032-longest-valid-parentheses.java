a6e4155ea36919995ed2b367a472e5909f8a81fe
VIGNESHKUMAR RADHAKRISHNAN <vigneshkumarponnamaravathy@gmail.com>
2024-01-01 15:36:41 +0530

    Time: 3 ms (71.52%), Space: 43.2 MB (18.85%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.java
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.java
@@ -1,6 +1,6 @@
 class Solution {
     public int longestValidParentheses(String s) {
-        int sumOfParentheses = 0;
+        int sum = 0;
         Stack<Integer> result = new Stack<>();
         result.push(-1);
         for(int i=0;i<s.length();i++){
@@ -11,9 +11,9 @@ class Solution {
                 if(result.isEmpty())
                     result.push(i);
                 else
-                    sumOfParentheses = Math.max(sumOfParentheses, i-result.peek());
+                    sum = Math.max(sum, i-result.peek());
             }
         }
-        return sumOfParentheses;
+        return sum;
     }
 }

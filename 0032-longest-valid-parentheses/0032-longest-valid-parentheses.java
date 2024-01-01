e2375599951f08a3db748c32546c05b43b5064ab
VIGNESHKUMAR RADHAKRISHNAN <vigneshkumarponnamaravathy@gmail.com>
2024-01-01 15:35:24 +0530

    Time: 8 ms (6.75%), Space: 43.1 MB (23.05%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.java
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.java
@@ -1,12 +1,9 @@
 class Solution {
     public int longestValidParentheses(String s) {
-        int n=s.length();
-        if(s==null || n == 0)
-            return 0;
         int sumOfParentheses = 0;
         Stack<Integer> result = new Stack<>();
         result.push(-1);
-        for(int i=0;i<n;i++){
+        for(int i=0;i<s.length();i++){
             if(s.charAt(i) == '(')
                 result.push(i);
             else{

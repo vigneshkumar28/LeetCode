3960335e045ed690902936974c281ac81d18215d
VIGNESHKUMAR RADHAKRISHNAN <vigneshkumarponnamaravathy@gmail.com>
2024-01-01 23:14:29 +0530

    Time: 8 ms (98.82%), Space: 45.7 MB (6.13%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.java
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -2,16 +2,14 @@ class Solution {
     public int findContentChildren(int[] g, int[] s) {
         Arrays.sort(g);
         Arrays.sort(s);
-        int i=0,j=0, contentChildren = 0;
+        int i=0,j=0, count = 0;
         while(i<g.length && j<s.length){
             if(s[j] >= g[i]){
-                contentChildren++;
+                count++;
                 i++;
-                j++;
-            }else{
-                j++;
             }
+            j++;
         }
-        return contentChildren;
+        return count;
     }
 }

674edbeaabfbfbd9a88b6a3e4a617d7b2e4898d7
VIGNESHKUMAR RADHAKRISHNAN <vigneshkumarponnamaravathy@gmail.com>
2024-01-12 23:34:12 +0530

    Time: 12 ms (82.06%), Space: 45.4 MB (37.65%) - LeetHub

---
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String removeDuplicates(String s) {
+        //stack based logic
+        StringBuilder stack = new StringBuilder();
+        for(char c: s.toCharArray()){
+            if(stack.length() > 0 && stack.charAt(stack.length()-1) == c)
+                stack.deleteCharAt(stack.length()-1);
+            else
+                stack.append(c);
+        }
+        return stack+"";
+    }
+}

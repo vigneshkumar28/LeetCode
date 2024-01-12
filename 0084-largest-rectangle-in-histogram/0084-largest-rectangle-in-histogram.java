263fbc45039b8af82f870d9eaa765046a2f6cbfa
VIGNESHKUMAR RADHAKRISHNAN <vigneshkumarponnamaravathy@gmail.com>
2024-01-12 22:40:48 +0530

    Time: 120 ms (17.05%), Space: 57.3 MB (44.55%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -19,9 +19,6 @@ class Solution {
             rightBoundary[i] = stack.isEmpty()? n-1 : stack.peek()-1;
             stack.push(i);
         }
-        // System.out.println(Arrays.toString(heights));
-        // System.out.println(Arrays.toString(rightBoundary));
-        // System.out.println(Arrays.toString(leftBoundary));
         int maxHistogramRectangle = 0;
         for(int i=0; i<n;i++){
             int width = rightBoundary[i]-leftBoundary[i]+1;

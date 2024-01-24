fbc75354244f858e8434ac6048cee5645e4d2aa4
VIGNESHKUMAR RADHAKRISHNAN <vigneshkumarponnamaravathy@gmail.com>
2024-01-24 23:11:31 +0530

    Time: 5 ms (100.00%), Space: 69.6 MB (68.33%) - LeetHub

--- 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.java
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.java
@@ -14,27 +14,20 @@
  * }
  */
 class Solution {
+    int count = 0;
     public int pseudoPalindromicPaths (TreeNode root) {
-        //use Deque instead of Stack class for Stack operations because Deque is faster than stack and LinkedList queue
-        Deque<Pair<TreeNode, Integer>> queue = new ArrayDeque();
-        queue.offer(new Pair(root, 0));
-        int path=0;
-        int count=0;
-        while(!queue.isEmpty()){
-            Pair<TreeNode, Integer> freq = queue.pop();
-            TreeNode keyNode = freq.getKey();
-            path = freq.getValue();
-            if(keyNode != null){
-                path = path^(1<<keyNode.val);
-                if(keyNode.left == null && keyNode.right == null){
-                    if((path &(path-1))==0)
-                        ++count;
-                }else{
-                    queue.offer(new Pair(keyNode.right, path));
-                    queue.offer(new Pair(keyNode.left, path));
-                }
-            }
-        }
+        dfs(root, 0);
         return count;
+    }
+    private void dfs(TreeNode node, int isThere){
+        if(node == null)
+            return;
+        isThere = isThere ^ (1<<node.val);
+        if(node.left==null && node.right==null){
+            if((isThere & (isThere-1)) == 0)
+                count++;
+        }
+        dfs(node.right, isThere);
+        dfs(node.left, isThere);
     }
 }

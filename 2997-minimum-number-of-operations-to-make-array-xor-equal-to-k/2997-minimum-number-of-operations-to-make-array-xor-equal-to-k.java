72b129c9686928ad4fda5a89dd1f0a007ca6d7db
VIGNESHKUMAR RADHAKRISHNAN <vigneshkumarponnamaravathy@gmail.com>
2024-04-29 10:24:20 +0530

    Time: 1 ms (100.00%), Space: 56.8 MB (44.04%) - LeetHub

---
+++ 2997-minimum-number-of-operations-to-make-array-xor-equal-to-k/2997-minimum-number-of-operations-to-make-array-xor-equal-to-k.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int minOperations(int[] nums, int k) {
+        int finalXor=0;
+        for(int i: nums){
+            finalXor = finalXor^i;
+        }
+        int count=0;
+        while(k>0 || finalXor>0){
+            if((k%2) != (finalXor%2)){
+                count++;
+            }
+            k/=2;
+            finalXor/=2;
+        }
+        return count;
+    }
+}

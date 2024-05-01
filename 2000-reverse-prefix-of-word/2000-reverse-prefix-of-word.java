2f5134e8cca2398523b2979c61361d4efb438aba
VIGNESHKUMAR RADHAKRISHNAN <vigneshkumarponnamaravathy@gmail.com>
2024-05-01 16:42:19 +0530

    Time: 1 ms (49.93%), Space: 41.3 MB (91.13%) - LeetHub

--- 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.java
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.java
@@ -3,8 +3,7 @@ class Solution {
         int first= word.indexOf(ch);
         if(first==-1)
             return word;
-        StringBuffer sb= new StringBuffer(word.substring(0, first+1));
-        sb.reverse();
-        return sb + word.substring(first+1);
+        StringBuffer sb= new StringBuffer(word.substring(0, first+1));  
+        return sb.reverse().toString() + word.substring(first+1);
     }
 }

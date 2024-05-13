ee72b5cc404e7cd2e60b00ab8b8845ec2168d55c
VIGNESHKUMAR RADHAKRISHNAN <vigneshkumarponnamaravathy@gmail.com>
2024-05-13 16:28:14 +0530

    Time: 9 ms (100.00%), Space: 44.7 MB (81.08%) - LeetHub

--- 0956-tallest-billboard/0956-tallest-billboard.java
+++ 0956-tallest-billboard/0956-tallest-billboard.java
@@ -1,22 +1,18 @@
 class Solution {
-    int dp[][]=new int[21][5001+5000];
     public int tallestBillboard(int[] rods) {
-        int  n=rods.length;
-        for(int i=0;i<n;i++) Arrays.fill(dp[i],-1);
-        return solve(0,0,n,rods)/2;
-    }
-    public int solve(int diff,int index,int n,int []rods){
-        if(index>=n){
-            if(diff==0)
-            return 0;
-            return Integer.MIN_VALUE;
+        int sum = 0;
+        for(int r : rods) sum += r;
+        int[] dp = new int[sum + 1];
+        for(int i = 1; i < dp.length; ++i) dp[i] = -1;
+        for(int rod : rods) {
+            int[] prev = dp.clone();
+            for(int diff = 0; diff + rod <= sum; ++diff) {
+                if(prev[diff] < 0) continue;
+                int nd = Math.abs(diff - rod);
+                dp[nd] = Math.max(dp[nd], prev[diff] + Math.min(diff, rod));
+                dp[diff + rod] = Math.max(dp[diff + rod], prev[diff]);
+            }
         }
-        if(dp[index][diff+5000]!=-1)
-        return dp[index][diff+5000];
-        
-        int nottake=solve(diff,index+1,n,rods);
-        int take_long=rods[index]+solve(diff+rods[index],index+1,n,rods);
-        int take_short=rods[index]+solve(diff-rods[index],index+1,n,rods);
-        return dp[index][diff+5000]=Math.max(nottake,Math.max(take_long,take_short));
-       }
+        return dp[0];
+    }
 }

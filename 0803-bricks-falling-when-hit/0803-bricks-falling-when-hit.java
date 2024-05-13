957424e6ebd378ba22c8d6ca74520f9ea774ba6e
VIGNESHKUMAR RADHAKRISHNAN <vigneshkumarponnamaravathy@gmail.com>
2024-05-13 16:18:03 +0530

    Time: 9 ms (90.99%), Space: 57.2 MB (50.45%) - LeetHub

--- 0803-bricks-falling-when-hit/0803-bricks-falling-when-hit.java
+++ 0803-bricks-falling-when-hit/0803-bricks-falling-when-hit.java
@@ -1,97 +1,59 @@
 class Solution {
     public int[] hitBricks(int[][] grid, int[][] hits) {
-        int R = grid.length, C = grid[0].length;
-        int[] dr = {1, 0, -1, 0};
-        int[] dc = {0, 1, 0, -1};
-
-        int[][] A = new int[R][C];
-        for (int r = 0; r < R; ++r)
-            A[r] = grid[r].clone();
-        for (int[] hit: hits)
-            A[hit[0]][hit[1]] = 0;
-
-        UnionFind uf = new UnionFind(R*C + 1);
-        for (int r = 0; r < R; ++r) {
-            for (int c = 0; c < C; ++c) {
-                if (A[r][c] == 1) {
-                    int i = r * C + c;
-                    if (r == 0)
-                        uf.union(i, R*C);
-                    if (r > 0 && A[r-1][c] == 1)
-                        uf.union(i, (r-1) *C + c);
-                    if (c > 0 && A[r][c-1] == 1)
-                        uf.union(i, r * C + c-1);
-                }
+        int n = grid.length;
+        int m = grid[0].length;
+        int[] ans = new int[hits.length];
+        
+        for(int[] hit : hits) {
+            grid[hit[0]][hit[1]] *= -1;
+        }
+        for(int i = 0; i < m; i++) {
+            dfs(grid, 0, i);
+        }
+        for(int i = hits.length-1; i >= 0; i--) {
+            int x = hits[i][0];
+            int y = hits[i][1];
+            if(grid[x][y] == 0) {
+                continue;
+            } 
+            grid[x][y] = 1;
+            if(connected(grid, x, y, n, m)) {
+                ans[i] = dfs(grid, x, y)-1;
             }
         }
-        int t = hits.length;
-        int[] ans = new int[t--];
-
-        while (t >= 0) {
-            int r = hits[t][0];
-            int c = hits[t][1];
-            int preRoof = uf.top();
-            if (grid[r][c] == 0) {
-                t--;
-            } else {
-                int i = r * C + c;
-                for (int k = 0; k < 4; ++k) {
-                    int nr = r + dr[k];
-                    int nc = c + dc[k];
-                    if (0 <= nr && nr < R && 0 <= nc && nc < C && A[nr][nc] == 1)
-                        uf.union(i, nr * C + nc);
-                }
-                if (r == 0)
-                    uf.union(i, R*C);
-                A[r][c] = 1;
-                ans[t--] = Math.max(0, uf.top() - preRoof - 1);
-            }
-        }
-
         return ans;
     }
-}
-
-class UnionFind {
-    int[] parent;
-    int[] rank;
-    int[] sz;
-
-    public UnionFind(int N) {
-        parent = new int[N];
-        for (int i = 0; i < N; ++i)
-            parent[i] = i;
-        rank = new int[N];
-        sz = new int[N];
-        Arrays.fill(sz, 1);
-    }
-
-    public int find(int x) {
-        if (parent[x] != x) parent[x] = find(parent[x]);
-        return parent[x];
-    }
-
-    public void union(int x, int y) {
-        int xr = find(x), yr = find(y);
-        if (xr == yr) return;
-
-        if (rank[xr] < rank[yr]) {
-            int tmp = yr;
-            yr = xr;
-            xr = tmp;
+    public int dfs(int[][] grid, int x, int y) {
+        if(x < 0 || y < 0 || x >= grid.length || y >= grid[0].length || grid[x][y] != 1) {
+            return 0;
         }
-        if (rank[xr] == rank[yr])
-            rank[xr]++;
-
-        parent[yr] = xr;
-        sz[xr] += sz[yr];
+        grid[x][y] = 2;
+        return 1 + dfs(grid, x+1, y) + dfs(grid, x, y+1) + dfs(grid, x-1, y) + dfs(grid, x, y-1);
     }
-
-    public int size(int x) {
-        return sz[find(x)];
-    }
-
-    public int top() {
-        return size(sz.length - 1) - 1;
+    public boolean connected(int[][] grid, int x, int y, int n, int m) {
+        if(x == 0) {
+            return true;
+        }
+        if(x+1 < n) {
+            if(grid[x+1][y] == 2) {
+                return true;
+            }
+        }
+        if(x-1 >= 0) {
+            if(grid[x-1][y] == 2) {
+                return true;
+            }
+        }
+        if(y+1 < m) {
+            if(grid[x][y+1] == 2) {
+                return true;
+            }
+        }
+        if(y-1 >= 0) {
+            if(grid[x][y-1] == 2) {
+                return true;
+            }
+        }
+        return false;
     }
 }

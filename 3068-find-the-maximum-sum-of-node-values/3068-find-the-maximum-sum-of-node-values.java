e9a69be10e5161cfc60124f8055dfd5afcf25130
VIGNESHKUMAR RADHAKRISHNAN <vigneshkumarponnamaravathy@gmail.com>
2024-05-19 18:27:35 +0530

    Time: 1 ms (100.00%), Space: 54.4 MB (70.99%) - LeetHub

--- 3068-find-the-maximum-sum-of-node-values/3068-find-the-maximum-sum-of-node-values.java
+++ 3068-find-the-maximum-sum-of-node-values/3068-find-the-maximum-sum-of-node-values.java
@@ -1,25 +1,25 @@
 class Solution {
     public long maximumValueSum(int[] nums, int k, int[][] edges) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a , b) -> Integer.compare(b, a));
-
         long sum = 0;
-        for(int num : nums){
-            sum += (long) num;
-            int xorNum = num ^ k;
-            pq.offer(xorNum - num);
-        }
+        long minExtra = 1000000;
+        int count = 0;
 
-        while(pq.size() > 1){
-            int diff1 = pq.poll();
-            int diff2 = pq.poll();
-            
-            if(diff1 + diff2 > 0) {
-                sum += diff1 + diff2;
-            } else {
-                break;
+        for(int val : nums){
+            if((val ^ k) > val){
+                sum += val ^ k;
+                minExtra = Math.min(minExtra, (val ^ k)- val);
+                count++;
+            }
+            else{
+                sum += val;
+                minExtra = Math.min(minExtra, val - (val ^ k));
             }
         }
 
-        return sum;
+        if(count %2 ==0){
+            return sum;
+        } else{
+            return sum - minExtra;
+        }
     }
 }

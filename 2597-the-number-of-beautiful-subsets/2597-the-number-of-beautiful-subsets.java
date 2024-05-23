ab40be062ce31daf7575e4b8474ca96380eae226
VIGNESHKUMAR RADHAKRISHNAN <vigneshkumarponnamaravathy@gmail.com>
2024-05-23 08:55:34 +0530

    Time: 4 ms (99.28%), Space: 43.8 MB (92.31%) - LeetHub

--- 2597-the-number-of-beautiful-subsets/2597-the-number-of-beautiful-subsets.java
+++ 2597-the-number-of-beautiful-subsets/2597-the-number-of-beautiful-subsets.java
@@ -1,31 +1,21 @@
 class Solution {
     public int beautifulSubsets(int[] nums, int k) {
-        int totalCount=1;
-        Map<Integer, Map<Integer, Integer>> freqMap = new TreeMap<>();
-        for(int num: nums){
-            Map<Integer, Integer> remainderFreq = freqMap.getOrDefault(num%k, new TreeMap<>());
-            remainderFreq.put(num, remainderFreq.getOrDefault(num, 0)+1);
-            freqMap.put(num%k, remainderFreq);
-        }
-        for(Map.Entry<Integer, Map<Integer, Integer>>  entry: freqMap.entrySet()){
-            int prevNum = -k, prev2=0, prev1=1, curr=1;
-            for(Map.Entry<Integer, Integer> freqEntry: entry.getValue().entrySet()){
-                int num= freqEntry.getKey();
-                int freq = freqEntry.getValue();
-                int skip=prev1;
-                int take;
-                if(num - prevNum == k){
-                    take = ((1<<freq)-1)*prev2;
-                }else{
-                    take = ((1<<freq)-1)*prev1;
-                }
-                curr = skip+take;
-                prev2 = prev1;
-                prev1 = curr;
-                prevNum = num;
+        Map<Integer, Integer> m = new HashMap<>();
+        for(int num : nums)
+            m.put(num, m.getOrDefault(num, 0) + 1);
+        int res = 1, prev = 0, prevPrev = 0;
+        for(Map.Entry<Integer, Integer> e : m.entrySet()) {
+            int cur = e.getKey();
+            if(m.containsKey(cur - k)) continue;
+            prev = 0;
+            while(m.containsKey(cur)) {
+                prevPrev = prev;
+                prev = ((1 << m.get(cur)) - 1) * res;
+                res += prevPrev;
+                cur += k;
             }
-            totalCount *= curr;
+            res += prev;
         }
-        return totalCount-1;
+        return res - 1;
     }
 }

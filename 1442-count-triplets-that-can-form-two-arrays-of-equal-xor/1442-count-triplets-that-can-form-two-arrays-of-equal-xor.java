9faa809d57ea24d1cb644089a237a1da7a5f057f
VIGNESHKUMAR RADHAKRISHNAN <vigneshkumarponnamaravathy@gmail.com>
2024-05-30 10:11:30 +0530

    Time: 2 ms (77.44%), Space: 41.2 MB (42.05%) - LeetHub

--- 1442-count-triplets-that-can-form-two-arrays-of-equal-xor/1442-count-triplets-that-can-form-two-arrays-of-equal-xor.java
+++ 1442-count-triplets-that-can-form-two-arrays-of-equal-xor/1442-count-triplets-that-can-form-two-arrays-of-equal-xor.java
@@ -1,18 +1,19 @@
 class Solution {
     public int countTriplets(int[] arr) {
+        //O(n) time complecity and O(n) space complexity - in case space not matter
+        //One pass prefix xor
         int count=0;
-        //n^3 time complexity, 1 space complexity
-        for(int start=0;start<arr.length; start++){
-            int xorA=0;
-            for(int mid=start+1; mid<arr.length; mid++){
-                xorA ^= arr[mid-1];
-                int xorB = 0;
-                for(int end=mid;end<arr.length; end++){
-                    xorB ^= arr[end];
-                    if(xorA == xorB)
-                        count++;
-                }
-            }
+        int prefix=0;
+        Map<Integer, Integer> countMap= new HashMap<>();
+        Map<Integer, Integer> totalMap= new HashMap<>();
+        
+        countMap.put(0, 1);
+        for(int i=0;i<arr.length; i++){
+            prefix ^= arr[i];
+            count += countMap.getOrDefault(prefix, 0)*i - totalMap.getOrDefault(prefix, 0);
+            //map update
+            totalMap.put(prefix, totalMap.getOrDefault(prefix, 0)+i+1);
+            countMap.put(prefix, countMap.getOrDefault(prefix, 0)+1);
         }
         return count;
     }

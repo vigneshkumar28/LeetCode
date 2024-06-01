7a6c02ea2c4cfd9ecf26fc67f2caf92bd1f6baf7
VIGNESHKUMAR RADHAKRISHNAN <vigneshkumarponnamaravathy@gmail.com>
2024-06-01 11:17:59 +0530

    Time: 1 ms (99.87%), Space: 41.8 MB (76.71%) - LeetHub

--- 3110-score-of-a-string/3110-score-of-a-string.java
+++ 3110-score-of-a-string/3110-score-of-a-string.java
@@ -1,8 +1,8 @@
 class Solution {
     public int scoreOfString(String s) {
         int sum = 0;
-        for(int i=0;i<s.length()-1; i++){
-            sum += Math.abs((s.charAt(i)-'0') - (s.charAt(i+1)-'0'));
+        for(int i=1;i<s.length(); i++){
+            sum += Math.abs(s.charAt(i)-s.charAt(i-1));
         }
         return sum;
     }

3c4738dd55d2243347fe1d3d941149144209781e
VIGNESHKUMAR RADHAKRISHNAN <vigneshkumarponnamaravathy@gmail.com>
2024-06-22 09:31:54 +0530

    Time: 10 ms (80.88%), Space: 54.9 MB (58.69%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
@@ -1,15 +1,16 @@
 class Solution {
     public int numberOfSubarrays(int[] nums, int k) {
-        int currSum =0;
-        int subArrays=0;
-        Map<Integer, Integer> prefixSum = new HashMap<>();
-        prefixSum.put(currSum,  1);
-        for(int i=0;i<nums.length; i++){
-            currSum += nums[i]%2;
-            if(prefixSum.containsKey(currSum-k)){
-                subArrays += prefixSum.get(currSum -k);
+        return maxK(nums, k)- maxK(nums,k-1);
+    }
+    private int maxK(int[] nums, int k){
+        int windowSize=0, subArrays=0, start=0;
+        for(int end=0;end<nums.length;end++){
+            windowSize += nums[end]%2;
+            while(windowSize > k){
+                windowSize -= nums[start]%2;
+                start++;
             }
-            prefixSum.put(currSum, prefixSum.getOrDefault(currSum,0)+1);
+            subArrays += end-start+1;
         }
         return subArrays;
     }

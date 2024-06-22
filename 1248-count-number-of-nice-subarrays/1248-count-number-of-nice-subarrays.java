40e388f080043af847a8d2c190b6df336229f13d
VIGNESHKUMAR RADHAKRISHNAN <vigneshkumarponnamaravathy@gmail.com>
2024-06-22 09:32:58 +0530

    Time: 3 ms (100.00%), Space: 52.6 MB (97.37%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
@@ -1,17 +1,16 @@
 class Solution {
     public int numberOfSubarrays(int[] nums, int k) {
-        return maxK(nums, k)- maxK(nums,k-1);
-    }
-    private int maxK(int[] nums, int k){
-        int windowSize=0, subArrays=0, start=0;
-        for(int end=0;end<nums.length;end++){
-            windowSize += nums[end]%2;
-            while(windowSize > k){
-                windowSize -= nums[start]%2;
-                start++;
+        int n = nums.length;
+        int[] freq = new int[n + 1];
+        freq[0] = 1;
+        int ans = 0, t = 0;
+        for (int v : nums) {
+            t += v & 1;
+            if (t - k >= 0) {
+                ans += freq[t - k];
             }
-            subArrays += end-start+1;
+            freq[t]++;
         }
-        return subArrays;
+        return ans;
     }
 }

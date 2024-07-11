e1f2461f30cd07a76b422014f310891e7cef7674
VIGNESHKUMAR RADHAKRISHNAN <vigneshkumarponnamaravathy@gmail.com>
2024-07-11 09:30:37 +0530

    Time: 1 ms (99.49%), Space: 41.3 MB (90.75%) - LeetHub

---
+++ 1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.java
@@ -0,0 +1,34 @@
+class Solution {
+    public String reverseParentheses(String s) {
+         int n = s.length();
+        Stack<Integer> openParenthesesIndices = new Stack<>();
+        int[] pair = new int[n];
+
+        for (int i = 0; i < n; ++i) {
+            if (s.charAt(i) == '(') {
+                openParenthesesIndices.push(i);
+            }
+            if (s.charAt(i) == ')') {
+                int j = openParenthesesIndices.pop();
+                pair[i] = j;
+                pair[j] = i;
+            }
+        }
+
+        StringBuilder result = new StringBuilder();
+        for (
+            int currIndex = 0, direction = 1;
+            currIndex < n;
+            currIndex += direction
+        ) {
+            if (s.charAt(currIndex) == '(' || s.charAt(currIndex) == ')') {
+                currIndex = pair[currIndex];
+                direction = -direction;
+            } else {
+                result.append(s.charAt(currIndex));
+            }
+        }
+
+        return result.toString();
+    }
+}

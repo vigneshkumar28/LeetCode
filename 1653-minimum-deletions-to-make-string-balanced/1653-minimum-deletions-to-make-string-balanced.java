fe7b6734bad869c1f2c320d27b026341339e38ca
VIGNESHKUMAR RADHAKRISHNAN <vigneshkumarponnamaravathy@gmail.com>
2024-07-30 10:10:10 +0530

    Time: 44 ms (25.07%), Space: 45.9 MB (5.01%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.java
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.java
@@ -1,16 +1,15 @@
 class Solution {
     public int minimumDeletions(String s) {
         int n=s.length();
-        int[] countA = new int[n];
         int aCount=0;
         for(int i=n-1; i>=0; i--){
-            countA[i]=aCount;
             if(s.charAt(i)=='a')aCount++;
         }
         int minDeletions=n;
         int bCount=0;
         for(int i=0;i<n;i++){
-            minDeletions = Math.min(minDeletions, bCount+countA[i]);
+            if(s.charAt(i) == 'a') aCount--;
+            minDeletions = Math.min(minDeletions, aCount+bCount);
             if(s.charAt(i) == 'b') bCount++;
         }
         return minDeletions;

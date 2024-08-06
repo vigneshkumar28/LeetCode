48bb1c364a4b4c8a9ee0b0f51625a6feca08f1b2
VIGNESHKUMAR RADHAKRISHNAN <vigneshkumarponnamaravathy@gmail.com>
2024-08-06 11:24:41 +0530

    Time: 7 ms (100.00%), Space: 45.8 MB (17.31%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
@@ -1,18 +1,23 @@
 class Solution {
     public int minimumPushes(String word) {
-        Map<Character, Integer> freqMap = new HashMap<>();
-        for(char c: word.toCharArray()){
-            freqMap.put(c, freqMap.getOrDefault(c, 0)+1);
+        int[] arr = new int[26];
+        for(int i=0;i<word.length();i++){
+            arr[word.charAt(i) - 'a']++;
         }
-        //stor in descending order freq
-        PriorityQueue<Integer> minHeap= new PriorityQueue<>((a, b) -> b-a);
-        minHeap.addAll(freqMap.values());
-        int totalPushes=0;
-        int index=0;
-        while(!minHeap.isEmpty()){
-            totalPushes += (index/8 + 1)*minHeap.poll();
-            index++;
+        Arrays.sort(arr);
+        int i=25;
+        int count = 0;
+        int start = 1;
+        int ans =0;
+        while( i >= 0 && arr[i] != 0){
+            ans+=(start * arr[i]);
+            count++;
+            if(count == 8){ 
+                start++;
+                count = 0;
+            }
+            i--;
         }
-        return totalPushes;
+        return ans;
     }
 }

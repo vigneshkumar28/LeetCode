475055d9ae19d6fcac66c7b7b22c1120a53a612d
VIGNESHKUMAR RADHAKRISHNAN <vigneshkumarponnamaravathy@gmail.com>
2024-09-02 13:30:19 +0530

    Time: 2 ms (72.59%), Space: 56.7 MB (98.90%) - LeetHub

--- 1894-find-the-student-that-will-replace-the-chalk/1894-find-the-student-that-will-replace-the-chalk.java
+++ 1894-find-the-student-that-will-replace-the-chalk/1894-find-the-student-that-will-replace-the-chalk.java
@@ -1,17 +1,25 @@
 class Solution {
     public int chalkReplacer(int[] chalk, int k) {
-        long sum=0;
-        for(int i=0;i<chalk.length; i++){
-            sum+= chalk[i];
-            if(sum >k) break;
+        int n=chalk.length;
+        long[] prefixSum= new long[n];
+        prefixSum[0]= chalk[0];
+        for(int i=1;i<n;i++){
+            prefixSum[i]= prefixSum[i-1]+chalk[i];
         }
-        k = k%(int)sum;
-        for(int i=0;i<chalk.length;i++){
-            if(k<chalk[i]){
-                return i;
+        long sum= prefixSum[n-1];
+        long remaningChalk = (k%sum);
+        return binarySeach(prefixSum, remaningChalk);
+    }
+    private int binarySeach(long[] arr, long tar){
+        int low=0, high= arr.length-1;
+        while(low< high){
+            int mid = low+(high-low)/2;
+            if(arr[mid]<=tar){
+                low = mid+1;
+            }else{
+                high=mid;
             }
-            k = k-chalk[i];
         }
-        return 0;
+        return high;
     }
 }
